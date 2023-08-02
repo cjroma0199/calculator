a63abf7d126a7b269b059f45ced0f76764303e16
cjrdelosreyes <122694227+cjroma0199@users.noreply.github.com>
2023-08-02 13:26:48 +0800

    Add delete function

--- calculator.js
+++ calculator.js
@@ -1,11 +1,27 @@
 const MAX_CHARACTERS = 13;
 const buttons = document.querySelectorAll('.button');
 const currentScreen = document.querySelector('#currentScreen');
+const deleteImg = document.querySelector('#deleteImg');
+
+deleteImg.addEventListener('click', deleteChar);
 
 buttons.forEach((button) =>
-  button.addEventListener('click', (e) => console.log(isOperator(e)))
+  button.addEventListener('click', (e) => {
+    let char = getChar(e);
+    console.log(e);
+    if (isScreenFull() && !isDelete(e)) return;
+    if (isNumber(char) || (char == '.' && !isDecimalPresent())) {
+      addToLowerScreen(char);
+    }
+
+    if (isDelete(e)) return deleteChar();
+  })
 );
-window.addEventListener('keydown', (e) => console.log(isOperator(e)));
+window.addEventListener('keydown', (e) => {
+  if (isScreenFull) return;
+  if (isNumber(getChar(e)) || (getChar(e) == '.' && isDecimalPresent())) {
+  }
+});
 
 function isScreenFull() {
   return currentScreen.textContent.length == MAX_CHARACTERS;
@@ -16,6 +32,14 @@ function isDelete(e) {
   return e.key == 'Backspace';
 }
 
+function isNumber(char) {
+  return Boolean(Number(char));
+}
+
+function isDecimalPresent() {
+  return currentScreen.textContent.includes('.');
+}
+
 function getChar(e) {
   if (e instanceof MouseEvent) return e.target.dataset.button;
   return e.key;
@@ -24,4 +48,21 @@ function getChar(e) {
 function isOperator(e) {
   let operators = '+-*/';
   return operators.includes(getChar(e));
+}
+
+function addToLowerScreen(char) {
+  if (currentScreen.textContent === '0' && char != '.') {
+    currentScreen.textContent = char;
+    return;
+  }
+
+  currentScreen.textContent += char;
+  return;
+}
+
+function deleteChar() {
+  let screenValue = currentScreen.textContent;
+  if (screenValue.length === 1) return (currentScreen.textContent = 0);
+  console.log(screenValue);
+  currentScreen.textContent = screenValue.slice(0, screenValue.length - 1);
 }
