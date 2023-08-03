ba49a7213eda21a8b36c630160a1bba691707a62
cjrdelosreyes <122694227+cjroma0199@users.noreply.github.com>
2023-08-03 15:50:30 +0800

    Add calculation

--- calculator.js
+++ calculator.js
@@ -1,20 +1,20 @@
-const MAX_CHARACTERS = 13;
+const MAX_CHARACTERS = 12;
 const buttons = document.querySelectorAll('.button');
 const currentScreen = document.querySelector('#currentScreen');
+const previousScreen = document.querySelector('#previousScreen');
 const deleteImg = document.querySelector('#deleteImg');
 
 deleteImg.addEventListener('click', deleteChar);
 
+const operandsAndOperator = {
+  firstNum: null,
+  operator: null,
+  secondNum: null,
+};
+
 buttons.forEach((button) =>
   button.addEventListener('click', (e) => {
-    let char = getChar(e);
-    console.log(e);
-    if (isScreenFull() && !isDelete(e)) return;
-    if (isNumber(char) || (char == '.' && !isDecimalPresent())) {
-      addToLowerScreen(char);
-    }
-
-    if (isDelete(e)) return deleteChar();
+    app(e);
   })
 );
 window.addEventListener('keydown', (e) => {
@@ -22,6 +22,76 @@ window.addEventListener('keydown', (e) => {
   if (isNumber(getChar(e)) || (getChar(e) == '.' && isDecimalPresent())) {
   }
 });
+
+function operate({ firstNum, operator, secondNum }) {
+  firstNum = +firstNum;
+  secondNum = +secondNum;
+  console.log([firstNum, operator, secondNum]);
+  switch (operator) {
+    case '+':
+      return firstNum + secondNum;
+    case '-':
+      return firstNum - secondNum;
+    case '*':
+      return firstNum * secondNum;
+    case '/':
+      return firstNum / secondNum;
+    case '%':
+      return firstNum % secondNum;
+    default:
+      return null;
+  }
+}
+
+function app(e) {
+  let char = getChar(e);
+
+  if (isDelete(e)) return deleteChar();
+
+  if (isOperator(e)) {
+    if (!operandsAndOperator.operator) operandsAndOperator.operator = getChar(e);
+    if (previousScreen.textContent) {
+      operandsAndOperator.secondNum = currentScreen.textContent;
+    } else {
+      operandsAndOperator.firstNum = currentScreen.textContent;
+      updatePreviousScreen(operandsAndOperator);
+      clearScreen(currentScreen);
+    }
+
+    if (operandsAndOperator.firstNum && operandsAndOperator.secondNum) {
+      operandsAndOperator.firstNum = operate(operandsAndOperator);
+      operandsAndOperator.operator = getChar(e);
+      updatePreviousScreen(operandsAndOperator);
+      clearScreen(currentScreen);
+    }
+  }
+  console.log(e);
+  if (isScreenFull()) return;
+  if (isNumber(char) || (char == '.' && !isDecimalPresent())) {
+    addToLowerScreen(char);
+  }
+}
+
+function updatePreviousScreen(data) {
+  return (previousScreen.textContent = `${data.firstNum} ${operator.convert(
+    data.operator
+  )}`);
+}
+
+const operator = {
+  '+': '+',
+  '-': '-',
+  '*': '×',
+  '/': '÷',
+  '%': '%',
+  convert(operator) {
+    return this[operator];
+  },
+};
+
+function clearScreen(screen) {
+  return (screen.textContent = '');
+}
 
 function isScreenFull() {
   return currentScreen.textContent.length == MAX_CHARACTERS;
@@ -33,7 +103,7 @@ function isDelete(e) {
 }
 
 function isNumber(char) {
-  return Boolean(Number(char));
+  return '1234567890'.includes(Math.abs(char));
 }
 
 function isDecimalPresent() {
@@ -46,7 +116,7 @@ function getChar(e) {
 }
 
 function isOperator(e) {
-  let operators = '+-*/';
+  let operators = '+-*/%';
   return operators.includes(getChar(e));
 }
 
